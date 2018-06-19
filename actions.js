a0be0321c5502903c5ce6c35e2c6c5e2d7b74da3
Tomasz Sokolowski <sokolowskit@gmail.com>
2018-06-19 23:52:49 +0200

    Add file

---
+++ actions.js
@@ -0,0 +1,88 @@
+import uuid from uuid;
+
+//typy acji
+
+const ADD_COMMENT = 'ADD_COMMENT';
+
+{
+    type: ADD_COMMENT,
+    text: 'My first comment !'
+}
+
+const EDIT_COMMENT = 'EDIT_COMMENT';
+
+{
+    type: EDIT_COMMENT,
+    id: 13,
+    text: 'some new text'
+}
+
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+{
+    type: REMOVE_COMMENT,
+    id: 9
+}
+
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+
+{
+    type: THUMB_UP_COMMENT,
+    id: 3
+}
+
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+{
+    type: THUMB_UP_COMMENT,
+    id: 3
+}
+
+
+
+
+const boundAddComment = text => dispatch(addComment(text));
+const boundEditComment = (text, id) => dispatch(editComment(text, id));
+const boundRemoveComment = id => dispatch(removeComment(id));
+const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+const boundThumbDownComment = id => dispatch(thumbDownComment(id));
+
+
+// kreatory akcji
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(text, id) {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id
+    }
+}
+
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id
+    }
+}
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id
+    }
+}
